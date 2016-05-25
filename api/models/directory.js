131b0661a79835abf289db1d34444265a061039f
Austin Miller <amiller.utah@gmail.com>
2016-05-25 09:25:17 -0600

    Created view for directory and modified schema on backend

--- api/models/directory.js
+++ api/models/directory.js
@@ -2,10 +2,14 @@ var mongoose = require('mongoose');
 
 var schema = new mongoose.Schema({
 	name: { type: String, require: true },
-	phone: { type: Number, require: true },
 	department: { type: String },
+	phone: { type: Number, require: true },
+	ext: { type: Number },
 	cell: { type: Number },
-	description: { type: String },
+	email: { type: String },
+	imagepath: { type: String },
+	note: { type: String },
+	status: { type: String },
 	created_at: { type: Date, default: Date.now }
 });
 
